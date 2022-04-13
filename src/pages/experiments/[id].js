d75bc0c6fe256b9291cc22d0e043009015d67d74
John Beresford <jberesford@volcaus.com>
2022-04-12 19:05:45 -0700

    etc

--- src/pages/experiments/[id].js
+++ src/pages/experiments/[id].js
@@ -1,9 +1,21 @@
 import dynamic from 'next/dynamic';
 import getData from '@/helpers/data';
 import Link from 'next/link';
-import { useEffect } from 'react';
+import { Suspense, useEffect } from 'react';
 import useStore from '@/helpers/store';
 import { Stats } from '@react-three/drei';
+
+const LoadingExperiment = () => {
+  useEffect(() => {
+    useStore.setState({ experimentLoaded: false });
+
+    return () => {
+      useStore.setState({ experimentLoaded: true });
+    };
+  }, []);
+
+  return <></>;
+};
 
 // dom components goes here
 const DOM = ({ experiment }) => {
@@ -37,6 +49,23 @@ const DOM = ({ experiment }) => {
           <h1 style={{ pointerEvents: 'none', touchAction: 'none' }}>
             {experiment.name.toLowerCase()}
           </h1>
+
+          <div
+            className='codeBtn'
+            style={{
+              backgroundColor: experiment.themeColor,
+              boxShadow: `0px 0px 10px 2px ${experiment.hudColor}88`,
+            }}
+          >
+            <a
+              style={{ color: experiment.hudColor }}
+              rel='noreferrer'
+              target='_blank'
+              href={`https://github.com/JMBeresford/lab/tree/main/src/components/canvas/experiments/${experiment.page}`}
+            >
+              {'</>'}
+            </a>
+          </div>
         </div>
       </div>
     </>
@@ -52,7 +81,9 @@ const R3F = ({ experiment }) => {
 
   return (
     <>
-      <Experiment />
+      <Suspense fallback={<LoadingExperiment />}>
+        <Experiment />
+      </Suspense>
     </>
   );
 };
