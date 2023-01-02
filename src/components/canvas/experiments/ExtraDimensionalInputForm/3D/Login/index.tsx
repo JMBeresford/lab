de6c4882b5bdd5944948b77732bcb5efe95695da
John Beresford <volcaus@gmail.com>
2023-01-02 12:59:35 -0800

    fix mobile view for 3d input experiment

--- src/components/canvas/experiments/ExtraDimensionalInputForm/3D/Login/index.tsx
+++ src/components/canvas/experiments/ExtraDimensionalInputForm/3D/Login/index.tsx
@@ -1,6 +1,6 @@
 import { Text, useCursor } from "@react-three/drei";
-import { Euler, Vector3 } from "@react-three/fiber";
-import { useState } from "react";
+import { Euler, Vector3, useThree } from "@react-three/fiber";
+import { useEffect, useMemo, useState } from "react";
 import { animated, useSpring } from "@react-spring/three";
 
 /**
@@ -9,6 +9,7 @@ import { animated, useSpring } from "@react-spring/three";
  */
 import { Input } from "@jberesford/r3f-input";
 import { useControls } from "leva";
+import { clamp } from "three/src/math/MathUtils";
 
 type PropsTypes = {
   position?: Vector3;
@@ -19,6 +20,7 @@ type PropsTypes = {
 const Login = (props: PropsTypes) => {
   const { width = 1.5, position, rotation } = props;
   const [hovered, setHovered] = useState(false);
+  const viewport = useThree((s) => s.viewport);
 
   const { buttonSize } = useSpring({
     buttonSize: hovered ? 1.025 : 1,
@@ -28,23 +30,30 @@ const Login = (props: PropsTypes) => {
     fontSize: { value: 0.0825, min: 0.01, max: 0.6, step: 0.01 },
   });
 
+  const w = useMemo(
+    () => clamp(width, 0.55, (viewport.width / viewport.distance) * 0.9),
+    [viewport, width]
+  );
+
   useCursor(hovered);
 
   return (
     <group position={position} rotation={rotation}>
       <Input
-        width={width}
+        width={w}
         type="text"
         label="Username"
-        textProps={{ fontSize }}
+        textProps={{ fontSize: fontSize * (w < 1.2 ? 0.55 : 1) }}
+        labelProps={{ fontSize: fontSize * (w < 1.2 ? 0.5 : 0.85) }}
       />
 
       <Input
-        width={width}
+        width={w}
         position={[0, -0.3, 0]}
         type="password"
         label="Password"
-        textProps={{ fontSize }}
+        textProps={{ fontSize: fontSize * (w < 1.2 ? 0.55 : 1) }}
+        labelProps={{ fontSize: fontSize * (w < 1.2 ? 0.5 : 0.85) }}
       />
 
       <group
@@ -53,7 +62,7 @@ const Login = (props: PropsTypes) => {
         onPointerLeave={() => setHovered(false)}
       >
         <animated.mesh scale={buttonSize} renderOrder={1}>
-          <boxGeometry args={[width, 0.15, 0.01]} />
+          <boxGeometry args={[w, 0.15, 0.01]} />
           <meshBasicMaterial color={"#7777ff"} transparent depthWrite={false} />
         </animated.mesh>
 
